9cfb4570934e5ce879d5a889332a3f6ba7b852eb
Joe Kratzat <joe.kratzat@gmail.com>
2020-02-12 22:42:55 -0500

    Add CRUD for sensors

--- temperature/src/main/java/com/temperature/db/SensorDao.java
+++ temperature/src/main/java/com/temperature/db/SensorDao.java
@@ -12,9 +12,10 @@ import com.temperature.core.SensorDo;
 
 public interface SensorDao {
 
-  @SqlUpdate("INSERT INTO sensors(room_id, name, created, modified) VALUES (:roomId, :name, :now, :now")
+  @SqlQuery("INSERT INTO sensors(room_id, name, created, modified) VALUES (:roomId, :name, :now, :now) returning *")
+  @RegisterRowMapper(SensorMapper.class)
   @Timestamped
-  public int create(@Bind("roomId") int roomId, @Bind("name") String name);
+  public SensorDo create(@Bind("roomId") int roomId, @Bind("name") String name);
 
   @SqlQuery("select * from sensors where room_id = :roomId")
   @RegisterRowMapper(SensorMapper.class)
@@ -23,5 +24,14 @@ public interface SensorDao {
   @SqlQuery("select * from sensors where id = :id")
   @RegisterRowMapper(SensorMapper.class)
   public SensorDo findById(@Bind("id") int id);
+
+  @SqlQuery("UPDATE sensors SET name = :name, modified = :now where id = :id returning *")
+  @RegisterRowMapper(SensorMapper.class)
+  @Timestamped
+  public SensorDo update(@Bind("id") int id, @Bind("name") String name);
+
+  @SqlUpdate("delete from sensors where room_id = :roomId and id = :id")
+  @Timestamped
+  public int delete(@Bind("roomId") int roomId, @Bind("id") int id);
 
 }
