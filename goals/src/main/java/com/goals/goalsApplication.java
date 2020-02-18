7b7d63bfcbab8b00775a5236115006f76beaa2f7
Joe Kratzat <joe.kratzat@gmail.com>
2020-02-17 21:48:25 -0500

    Setup basic db migration for users

--- goals/src/main/java/com/goals/goalsApplication.java
+++ goals/src/main/java/com/goals/goalsApplication.java
@@ -4,10 +4,16 @@ import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
-public class goalsApplication extends Application<goalsConfiguration> {
+import ch.qos.logback.core.db.DataSourceConnectionSource;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.jdbi3.JdbiFactory;
+import org.jdbi.v3.core.Jdbi;
+import io.dropwizard.migrations.MigrationsBundle;
+
+public class GoalsApplication extends Application<GoalsConfiguration> {
 
     public static void main(final String[] args) throws Exception {
-        new goalsApplication().run(args);
+        new GoalsApplication().run(args);
     }
 
     @Override
@@ -16,12 +22,17 @@ public class goalsApplication extends Application<goalsConfiguration> {
     }
 
     @Override
-    public void initialize(final Bootstrap<goalsConfiguration> bootstrap) {
-        // TODO: application initialization
+    public void initialize(final Bootstrap<GoalsConfiguration> bootstrap) {
+        bootstrap.addBundle(new MigrationsBundle<GoalsConfiguration>(){
+            @Override
+            public DataSourceFactory getDataSourceFactory(GoalsConfiguration config){
+                return config.getDataSourceFactory();
+            }
+        });
     }
 
     @Override
-    public void run(final goalsConfiguration configuration,
+    public void run(final GoalsConfiguration configuration,
                     final Environment environment) {
         // TODO: implement application
     }
--- goals/src/main/java/com/goals/goalsConfiguration.java
+++ goals/src/main/java/com/goals/goalsConfiguration.java
@@ -4,7 +4,21 @@ import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.*;
 import javax.validation.constraints.*;
+import javax.validation.Valid;
+import io.dropwizard.db.DataSourceFactory;
 
-public class goalsConfiguration extends Configuration {
-    // TODO: implement service configuration
+public class GoalsConfiguration extends Configuration {
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory(){
+        return this.database;
+    }
+
+    @JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+        this.database = dataSourceFactory;
+    }
 }
