0bb457456e6b10d147ac8c43a464cabdc6d056f9
Joe Kratzat <joe.kratzat@gmail.com>
2020-02-23 21:19:48 -0500

    Add Team table to migration

--- goals/src/main/java/com/goals/core/UserDo.java
+++ goals/src/main/java/com/goals/core/UserDo.java
@@ -16,16 +16,18 @@ public class UserDo {
   @JsonIgnore
   private final String passwordHash;
 
+  private int teamId;
   private final String confirmationToken;
   private final Timestamp confirmedAt;
   private final Timestamp lastLoginAt;
   private final Timestamp createdAt;
   private final Timestamp modifiedAt;
 
-  public UserDo(int id, String email, String passwordHash, String confirmationToken, Timestamp confirmedAt, Timestamp lastLoginAt, Timestamp createdAt, Timestamp modifiedAt) {
+  public UserDo(int id, String email, String passwordHash, int teamId, String confirmationToken, Timestamp confirmedAt, Timestamp lastLoginAt, Timestamp createdAt, Timestamp modifiedAt) {
       this.id = id;
       this.email = email;
       this.passwordHash = passwordHash;
+      this.teamId = teamId;
       this.confirmationToken = confirmationToken;
       this.confirmedAt = confirmedAt;
       this.lastLoginAt = lastLoginAt;
--- goals/src/main/java/com/goals/db/mapper/UserMapper.java
+++ goals/src/main/java/com/goals/db/mapper/UserMapper.java
@@ -13,6 +13,7 @@ public class UserMapper implements RowMapper<UserDo> {
   private static final String id = "id";
   private static final String email = "email";
   private static final String passwordHash = "password_hash";
+  private static final String teamId = "team_id";
   private static final String confirmationToken = "confirmation_token";
   private static final String confirmedAt = "confirmed_at";
   private static final String lastLoginAt = "last_login_at";
@@ -24,6 +25,7 @@ public class UserMapper implements RowMapper<UserDo> {
     return new UserDo(rs.getInt(id),
       rs.getString(email),
       rs.getString(passwordHash),
+      rs.getInt(teamId),
       rs.getString(confirmationToken),
       rs.getTimestamp(confirmedAt),
       rs.getTimestamp(lastLoginAt),
