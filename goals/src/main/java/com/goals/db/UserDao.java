ee9d5fe1d29f2c63c7c3f2ea7280f9855f4f71b4
Joe Kratzat <joe.kratzat@gmail.com>
2020-03-02 21:57:56 -0500

    Add confirmation to user

--- goals/src/main/java/com/goals/db/UserDao.java
+++ goals/src/main/java/com/goals/db/UserDao.java
@@ -1,9 +1,12 @@
 package com.goals.db;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
+import com.goals.core.GoalDo;
 import com.goals.core.UserDo;
+import com.goals.db.mapper.GoalMapper;
 import com.goals.db.mapper.UserMapper;
 
 import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
@@ -16,15 +19,23 @@ import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
 
 public interface UserDao {
 
-  @SqlQuery("INSERT into users(email, password_hash, team_id, created_at, modified_at) VALUES(:username, :password, :teamId, :now, :now) returning *")
+  @SqlQuery("INSERT into users(email, password_hash, team_id, confirmation_token, created_at, modified_at) VALUES(:username, :password, :teamId, :token, :now, :now) returning *")
   @RegisterRowMapper(UserMapper.class)
   @Timestamped
   @GetGeneratedKeys
-  public Optional<UserDo> create(@Bind("username") String username, @Bind("password") String password, @Bind("teamId") int teamId);
+  public Optional<UserDo> create(@Bind("username") String username, @Bind("password") String password, @Bind("teamId") int teamId, @Bind("token") String token);
 
   @SqlQuery("SELECT * from users where email = :username and confirmed_at is not null")
   @RegisterRowMapper(UserMapper.class)
   @Timestamped
   public Optional<UserDo> findByEmail(@Bind("username") String username);
 
+  @SqlQuery("SELECT * from users where confirmation_token = :token and confirmed_at is null")
+  @RegisterRowMapper(UserMapper.class)
+  @Timestamped
+  public Optional<UserDo> findByConfermationToke(@Bind("token") String token);
+
+  @SqlUpdate("UPDATE users set confirmation_token = null, confirmed_at = :now where id = :id and confirmed_at is null")
+  @Timestamped
+  public int confirm(@Bind("id") int id);
 }
